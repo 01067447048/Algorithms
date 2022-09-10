da819b42901c8fc84e06cce614d0b21ae99f72d6
jaehyeon <4wogus@gmail.com>
2022-09-10 12:25:04 +0900

    220910 LongestSubstringWithoutRepeatingCharacters.kt https://leetcode.com/problems/longest-substring-without-repeating-characters/

--- app/src/main/java/com/jaehyeon/algorithms/LongestSubstringWithoutRepeatingCharacters.kt
+++ app/src/main/java/com/jaehyeon/algorithms/LongestSubstringWithoutRepeatingCharacters.kt
@@ -1,7 +1,29 @@
 package com.jaehyeon.algorithms
 
-class Solution {
+class LongestSubstringWithoutRepeatingCharacters {
     fun lengthOfLongestSubstring(s: String): Int {
+        var tmp = ""
+        var max  = 0
 
+        //pwwkew
+
+        s.forEach { ch->
+            if (tmp.contains(ch)){
+                tmp = tmp.takeLastWhile {it != ch}
+                println(tmp)
+            }
+
+            tmp += ch
+
+            if (tmp.length > max){
+                max = tmp.length
+            }
+        }
+
+        return max
     }
+}
+
+fun main() {
+    println(LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring("pwwke"))
 }
